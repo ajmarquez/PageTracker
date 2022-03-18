509498da65265bcd5c9c13fa3db03b700ada5961
Abelardo Marquez <abelardo@hey.com>
2022-03-18 15:42:57 +0100

    Setups the project

---
+++ PageTracker/PageTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageTrackerApp.swift
+//  PageTracker
+//
+//  Created by Abelardo Marquez on 18.03.22.
+//
+
+import SwiftUI
+
+@main
+struct PageTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
